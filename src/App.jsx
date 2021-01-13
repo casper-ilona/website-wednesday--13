21685928da42fce4c929e7c7571b6af139f0fddb
Ilona <hair.masterpiece@gmail.com>
2021-01-13 17:23:22 +0300

    Добавила изминений

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,33 @@ function App() {
     <div className="App">
       <header className="App-header">
           Header
+          <div className={"Navigation"}>1</div>
+          <div className={"Navigation"}>2</div>
+          <div className={"Navigation"}>3</div>
+          <div className={"Navigation"}>4</div>
+          <div className={"Navigation"}>5</div>
+          <div className={"Navigation"}>6</div>
       </header>
-        <main className={"App-name"}>
+        <main className={"App-main"}>
             Main
+            <div className={"Main"}>
+              <input className={"Input"}/>
+            </div>
+            <div className={"Info-communication"}>
+            <div className={"Communication"}>1</div>
+            <div className={"Communication"}>2</div>
+            <div className={"Communication"}>3</div>
+            <div className={"Communication"}>4</div>
+            <div className={"Communication"}>5</div>
+            <div className={"Communication"}>6</div>
+            <div className={"Communication"}>7</div>
+            <div className={"Communication"}>8</div>
+            <div className={"Communication"}>9</div>
+            <div className={"Communication"}>10</div>
+            <div className={"Communication"}>11</div>
+            <div className={"Communication"}>12</div>
+            <div className={"Communication"}>13</div>
+            </div>
         </main>
         <footer className={"App-footer"}>
             Footer
