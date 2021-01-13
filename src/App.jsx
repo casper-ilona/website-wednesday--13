2ec9f6ac4a44ad4c8ea42f534924261f250f8dcd
Ilona <hair.masterpiece@gmail.com>
2021-01-13 16:57:30 +0300

    Сделала наброски будущего лаяута,добавила свойства главному контейнеру

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
+          Header
       </header>
+        <main className={"App-name"}>
+            Main
+        </main>
+        <footer className={"App-footer"}>
+            Footer
+        </footer>
     </div>
   );
 }
