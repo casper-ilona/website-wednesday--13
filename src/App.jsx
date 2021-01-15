fe7c8c1fc53638830d73a7d23f2a75e34c825ea2
Ilona <hair.masterpiece@gmail.com>
2021-01-15 17:55:32 +0300

    Расположила элементы согласна своему черновику

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,43 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          Header
-          <div className={"Navigation"}>1</div>
-          <div className={"Navigation"}>2</div>
-          <div className={"Navigation"}>3</div>
-          <div className={"Navigation"}>4</div>
-          <div className={"Navigation"}>5</div>
-          <div className={"Navigation"}>6</div>
-      </header>
-        <main className={"App-main"}>
-            Main
-            <div className={"Main"}>
-              <input className={"Input"}/>
-            </div>
-            <div className={"Info-communication"}>
-            <div className={"Communication"}>1</div>
-            <div className={"Communication"}>2</div>
-            <div className={"Communication"}>3</div>
-            <div className={"Communication"}>4</div>
-            <div className={"Communication"}>5</div>
-            <div className={"Communication"}>6</div>
-            <div className={"Communication"}>7</div>
-            <div className={"Communication"}>8</div>
-            <div className={"Communication"}>9</div>
-            <div className={"Communication"}>10</div>
-            <div className={"Communication"}>11</div>
-            <div className={"Communication"}>12</div>
-            <div className={"Communication"}>13</div>
-            </div>
-        </main>
-        <footer className={"App-footer"}>
-            Footer
-        </footer>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                Header
+                <div className={"Navigation"}>1</div>
+                <div className={"Navigation"}>2</div>
+                <div className={"Navigation"}>3</div>
+                <div className={"Navigation"}>4</div>
+                <div className={"Navigation"}>5</div>
+                <div className={"Navigation"}>6</div>
+            </header>
+            <main className={"App-main"}>
+                Main
+                <div className={"Main"}>
+                    <input className={"Input"}/>
+                </div>
+                <div className={"Info-communication"}>
+                    <div className={"Communication"}>1</div>
+                    <div className={"Communication"}>2</div>
+                    <div className={"Communication"}>3</div>
+                    <div className={"Communication"}>4</div>
+                    <div className={"Communication"}>5</div>
+                    <div className={"Communication"}>6</div>
+                    <div className={"Communication"}>7</div>
+                    <div className={"Communication"}>8</div>
+                    <div className={"Communication"}>9</div>
+                    <div className={"Communication"}>10</div>
+                    <div className={"Communication"}>11</div>
+                    <div className={"Communication"}>12</div>
+                    <footer className={"App-footer"}>
+                        Footer
+                    </footer>
+                </div>
+            </main>
+        
+        </div>
+    );
 }
 
 export default App;
